4d1562d1e90665471196f6779b57f6422f761e84
Yiu Fat Tse <yiu@wot.io>
2015-07-18 12:56:01 -0400

    giving more status for rewind

--- build_tape_report.sh
+++ build_tape_report.sh
@@ -6,8 +6,10 @@ bin_path=$(dirname ${0})
 log_path=$bin_path/log
 mkdir -p $log_path
 
-echo Rewinding....
-$bin_path/jump_to_beg_of_tape.sh
+status="Rewinding...."
+echo "$status"
+status="Still Rewinding...."
+$bin_path/jump_to_beg_of_tape.sh "$status"
 
 echo Generating Report onto $log_path/report_$date.log 
 $bin_path/jump_to_end_of_tape.sh >> $log_path/report_$date.log 2>&1
--- jump_to_beg_of_tape.sh
+++ jump_to_beg_of_tape.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
+status="$1"
 bin_path=$(dirname ${0})
 
 $bin_path/show_position.sh
 
 while $(mt -f /dev/nst0 bsf 1); do
+	echo "$status"
 	$bin_path/show_position.sh
 done
 
