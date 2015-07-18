1bee643cea40f17d111692fc63f1fe020a0e845c
Yiu Fat Tse <yiu@wot.io>
2015-07-18 17:38:16 -0400

    fixed typo

--- show_position.sh
+++ show_position.sh
@@ -3,7 +3,7 @@
 show_pos() {
 	status_result=$(mt -f /dev/nst0 status |grep File)
 	tell_result=$(mt -f /dev/nst0 tell)
-	block_postion=$( echo $tell_result | sed -e 's/^At block\ \(.*\).$/\1/' )
+	block_position=$( echo $tell_result | sed -e 's/^At block\ \(.*\).$/\1/' )
 	if [ "$D" == 1 ]; then
 		echo $tell_result
 		echo $status_result
