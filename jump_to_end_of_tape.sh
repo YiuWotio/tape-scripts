fdb36ad83c471935354a4557fc2be4544574349a
Yiu <test>
2015-07-19 22:22:23 -0400

    calculate session block size

--- jump_to_end_of_tape.sh
+++ jump_to_end_of_tape.sh
@@ -5,9 +5,16 @@ bin_path=$(dirname ${0})
 . $bin_path/helpers.sh
 
 show_pos
+prev_block=$block_position
 
 while $(mt -f /dev/nst0 fsf 1); do
 	show_pos
+	size=$(expr $block_position - $prev_block)
+	echo "Session block size: $size"
+
+	prev_block=$block_position
 done
 
 show_pos
+size=$(expr $block_position - $prev_block)
+echo "Session block size: $size"
