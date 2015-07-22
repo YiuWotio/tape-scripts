2bf6f3bf3f86cb4813d96c31c4e4ae8016e86929
Yiu <test>
2015-07-21 23:00:39 -0400

    sorted the lines from the tape of report

--- jump_to_end_of_tape.sh
+++ jump_to_end_of_tape.sh
@@ -4,17 +4,29 @@ bin_path=$(dirname ${0})
 
 . $bin_path/helpers.sh
 
+session_label=$( $bin_path/show_session_label.sh )
 show_pos
-prev_block=$block_position
 
+prev_block=$block_position
 while $(mt -f /dev/nst0 fsf 1); do
+	echo $tell_result
+	echo $status_result
+
 	show_pos
 	size=$(expr $block_position - $prev_block)
+
 	echo "Session block size: $size"
+	echo "Session label: $session_label"
+
+	session_label=$( $bin_path/show_session_label.sh )
 
 	prev_block=$block_position
 done
 
 show_pos
 size=$(expr $block_position - $prev_block)
+
 echo "Session block size: $size"
+echo $tell_result
+echo $status_result
+
