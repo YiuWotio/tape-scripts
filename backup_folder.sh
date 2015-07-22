6471943f8c0e04f97d2402e5d04d4b200cb16d41
Yiu <test>
2015-07-22 08:28:44 -0400

    adding time into session name

--- backup_folder.sh
+++ backup_folder.sh
@@ -7,9 +7,10 @@ fi
 
 # add code to jump to end before writing
 # add code to run backup in current position
+# generate reports with session info (tar file list catalog name, tape pos, block)
 
 bin_path=$(dirname ${0})
-date=$( date +%Y-%m-%d )
+timestamp=$( date +%Y-%m-%d-%H-%M )
 folder="$1"
 
 . $bin_path/helpers.sh
@@ -18,5 +19,5 @@ show_pos
 $bin_path/jump_to_end_of_tape.sh
 
 show_pos
-tar --label "${date}_${folder}" -cvf /dev/nst0 "$folder"
+tar --label "${timestamp}_${folder}" -cvf /dev/nst0 "$folder"
 show_pos
